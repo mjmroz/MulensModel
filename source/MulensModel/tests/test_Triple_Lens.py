894bc7847989a4b216a085d4ee38312bedb2358e
mj.mroz <mj.mroz@uw.edu.pl>
2026-05-24 02:29:07 +0200

    refactor: comment out unused VBM model code and update plotting for microjax magnification

--- source/MulensModel/tests/test_Triple_Lens.py
+++ source/MulensModel/tests/test_Triple_Lens.py
@@ -25,12 +25,12 @@ def test_VBM_vs_microjax():
                   't_E': 50.13, 't_0': 0, 's_31': 1.5, 'q_31': 0.000001, 'psi': np.degrees(-1.5)}
     t = np.linspace(parameters['t_0'] + tmin, parameters['t_0'] + tmax, num_points)
 
-    model_VBM = Model(parameters=parameters)
-    model_VBM.set_magnification_methods([float(min(t)), 'vbm_multiple', float(max(t))])
-    model_VBM.default_magnification_method = 'vbm_multiple'
-    magtriple_VBM = model_VBM.get_magnification(t)
-    model_VBM.update_caustics()
-    caustics_VBM = model_VBM.caustics
+    # model_VBM = Model(parameters=parameters)
+    # model_VBM.set_magnification_methods([float(min(t)), 'vbm_multiple', float(max(t))])
+    # model_VBM.default_magnification_method = 'vbm_multiple'
+    # magtriple_VBM = model_VBM.get_magnification(t)
+    # model_VBM.update_caustics()
+    # caustics_VBM = model_VBM.caustics
     x_VBM, y_VBM = caustics_VBM.get_caustics()
     x_critical_VBM, y_critical_VBM = caustics_VBM._critical_curve.x, caustics_VBM._critical_curve.y
 
@@ -46,17 +46,21 @@ def test_VBM_vs_microjax():
 
 
     if plot:
-        plt.scatter(x_VBM, y_VBM, color='r', label='VBM caustics', s=4, alpha=0.1, marker='x')
-        # plt.scatter(x_microjax, y_microjax, color='b', label='Microjax caustics', s=1, alpha=0.1, marker='o')
-        plt.scatter(
-            x_critical_VBM, y_critical_VBM, color='r', label='VBM critical curve', s=4, alpha=0.1, marker='x')
-        # plt.scatter(x_critical_microjax, y_critical_microjax, color='b', label='Microjax critical curve', s=1, alpha=0.1, marker='o')
-        plt.legend()
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.title('Caustics and critical curves for triple lens')
+        plt.plot(t, magtriple_microjax, color='b', label='Microjax magnification')
+        
+        # plt.scatter(x_VBM, y_VBM, color='r', label='VBM caustics', s=4, alpha=0.1, marker='x')
+        # # plt.scatter(x_microjax, y_microjax, color='b', label='Microjax caustics', s=1, alpha=0.1, marker='o')
+        # plt.scatter(
+        #     x_critical_VBM, y_critical_VBM, color='r', label='VBM critical curve', s=4, alpha=0.1, marker='x')
+        # # plt.scatter(x_critical_microjax, y_critical_microjax, color='b', label='Microjax critical curve', s=1, alpha=0.1, marker='o')
+        # plt.legend()
+        # plt.xlabel('x')
+        # plt.ylabel('y')
+        # plt.title('Caustics and critical curves for triple lens')
         plt.show()
 
+# The line `assert_almost_equal(magtriple_microjax[0], magtriple_VBM[0], decimal=3,
+# err_msg='Magnification')` is performing an assertion check in the Python test function.
     assert_almost_equal(magtriple_microjax[0], magtriple_VBM[0], decimal=3, err_msg='Magnification')
     # assert_almost_equal(x_microjax, x_VBM, decimal=3, err_msg='Caustics x')
     # assert_almost_equal(y_microjax, y_VBM, decimal=3, err_msg='Caustics y')
